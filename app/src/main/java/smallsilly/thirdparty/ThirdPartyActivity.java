627dc1622754cea93f5daceedd1c6eaaed750cb6
StringBOX <411600050@qq.com>
2017-03-22 09:42:19 +0800

    one

---
+++ app/src/main/java/smallsilly/thirdparty/ThirdPartyActivity.java
@@ -0,0 +1,13 @@
+package smallsilly.thirdparty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThirdPartyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_party);
+    }
+}
